61b704f523d8890e24df8963079f9d948ae97665
Github <Github@github.com>
2020-05-01 20:08:07 +0100

    Create portuguese.lua

---
+++ omg_framework/language/portuguese.lua
@@ -0,0 +1,29 @@
+Locales['portuguese'] = {
+	-- Welcome
+	["last_pos"] = "Tu foste colocado na tua ultima posição",
+	["welcome"] = "Bem Vindo, estamos a preparar o teu Personagem",
+	["welcome_back"] = "Bem Vindo de Volta, estamos a preparar o teu Personagem",
+	-- Logs
+	["new_user"] = "Novo Utilizador: ",
+	["user"] = "Utilizador: ",
+	-- Position
+	["position_save"] = "Posição Guardada",
+	["new_spawn_user"] = "Novo Spawn do Jogador: ",
+	["user_reconnect"] = "A Reconectar o Utilizador: ",
+	["user_reconnect2"] = "Gerar a última posição realizada",
+	-- Version
+	["startup"] = "^1A Tua Versão não está atualizada. Por Favor Vai a github.com/dev-omg para fazer o Download da nova Versão^2",
+	["startup2"] = "^1Versão Atual:^7 ",
+	["startup3"] = "^1Versão Atualizada:^7 ",	
+	["startup4"] = "^1A Tua Versão parece ser superior á versão atual^2",
+	["startup5"] = "^2Tudo a Funcionar Corretamente, OMG Framework Iniciada^2",
+	["settings"] = "^2Linguagem:^7 ",
+	["settings2"] = "^2Logs:^7 ",
+	-- Notifications
+	["remove_money_wallet"] = "Dinheiro da carteira removido",
+	["add_money_wallet"] = "Dinheiro Adicionado a Carteira",
+	["add_bank_money"] = "Dinheiro do Banco Adicionado",
+	["bank_money_removed"] = "Dinheiro do Banco Removido",
+	["remove_dirty_money"] = "Dinheiro Sujo Removido",
+	["add_diry_money"] = "Dinheiro Sujo Adicionado"
+}
