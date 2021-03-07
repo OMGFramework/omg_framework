af56deaf5959f8574446998d731cdfd09339f19c
Mackgame4 <fabiomagalhaes454@gmail.com>
2021-03-07 15:57:29 +0000

    + Files

--- locales/en.lua
+++ locales/en.lua
@@ -6,24 +6,12 @@ Locales['en'] = {
 	-- Logs
 	["new_user"] = "New User: ",
 	["user"] = "User: ",
-	-- Position
-	["position_save"] = "Position saved",
-	["new_spawn_user"] = "New spawn of user: ",
-	["user_reconnect"] = "Reconnecting the user: ",
-	["user_reconnect2"] = "Spawn last position performed",
 	-- Version
-	["startup"] = "^1Your version doesn't seem to be up to date. Please go to github.com/dev-omg to download the newest version^2",
+	["startup"] = "^1Your version doesn't seem to be up to date. Please go to www.github.com/OMGFramework/omg_framework to download the newest version^2",
 	["startup2"] = "^1Current Version:^7 ",
 	["startup3"] = "^1Updated Version:^7 ",	
 	["startup4"] = "^1Your version seems to be higher than the current version^2",
 	["startup5"] = "^2All working correcly, OMG Framework started^2",
 	["settings"] = "^2Language:^7 ",
 	["settings2"] = "^2Logs:^7 ",
-	-- Notifications
-	["remove_money_wallet"] = "Wallet money removed",
-	["add_money_wallet"] = "Wallet Money added",
-	["add_bank_money"] = "Bank Money added",
-	["bank_money_removed"] = "Bank Money removed",
-	["remove_dirty_money"] = "Dirty money removed",
-	["add_diry_money"] = "Dirty money added"
 }
--- version.lua
+++ version.lua
@@ -4,19 +4,22 @@ Citizen.CreateThread(function()
         local get_data_file = LoadResourceFile(GetCurrentResourceName(), "version.json")
         local version_json = json.decode(get_data_file).version
         if tostring(version_json) ~= tostring(data.version) and tostring(version_json) < tostring(data.version) then
+            -- Outdated
             print("^2------------------------OMG Information--------------------------------")
             print('' .. _L("startup") .. '')
-            print('^2[^1' .. Config.server_name .. "^2] https://github.com/dev-omg")
+            print('^2[^1' .. Config.server_name .. "^2] www.github.com/OMGFramework/omg_framework")
             print('' .. _L("startup2") .. '' .. version_json .. '')
 			print('' .. _L("startup3") .. '' .. data.version .. '')
             print('' .. _L("settings") .. '' .. tostring(Config.locale) .. '')
             print('' .. _L("settings2") .. '' .. tostring(Config.debug) .. '')
             print("^2--------------------------------------------------------------------------")
         elseif tostring(version_json) > tostring(data.version) then
+            -- Pre-Release
             print("^2--------------------------------------------------------------------------")
             print('' .. _L("startup5") .. '')
             print("^2--------------------------------------------------------------------------")
         else
+            -- Updated
             print("^2--------------------------------------------------------------------------")
             print('' .. _L("startup5") .. '')
             print('' .. _L("startup2") .. '' .. data.version .. '')
@@ -26,8 +29,5 @@ Citizen.CreateThread(function()
         end
         SetTimeout(600000, request_check_fivem_core_http)
     end
-    function request_check_fivem_core_http()
-        PerformHttpRequest("https://pastebin.com/raw/PWgev2cp", omg_framework_checker, "GET")
-    end
-    request_check_fivem_core_http()
+    PerformHttpRequest("https://omgframework.github.io/api/version.json", omg_framework_checker, "GET")
 end)
